308081f8f49f2886b607e6455e0498f68db36120
jol3588 <joehannlee@hotmail.com>
2023-07-04 20:00:58 -0700

    Basic layout

--- wall/src/App.js
+++ wall/src/App.js
@@ -1,25 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+
+import { ChakraProvider } from '@chakra-ui/react'
+import { Container } from '@chakra-ui/react'
+import { Box, Flex } from "@chakra-ui/react"
+import { Heading } from '@chakra-ui/react'
+import { AspectRatio } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to make fries.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Flex margin={8}>
+        <Heading>
+          Johann Lee
+        </Heading>
+      </Flex>
+
+      <Flex justify="center">
+        <Tabs variant='soft-rounded' colorScheme='green'>
+            <TabList>
+              <Tab>About</Tab>
+              <Tab>Experience</Tab>
+              <Tab>SoundCloud</Tab>
+              <Tab>Photography</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <p>
+                  Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, 
+                  Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, 
+                  Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, 
+                  Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, 
+                  Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here,
+                  Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, 
+                  Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, 
+                  Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, 
+                  Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, 
+                  Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here, Insert Bio here,    
+                </p>  
+              </TabPanel>
+              
+
+              <TabPanel></TabPanel>
+
+              <TabPanel>
+              <AspectRatio maxW='600px' ratio={1}>
+              <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1552526674&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+              </AspectRatio>
+              </TabPanel>
+
+              <TabPanel>
+                <p>Insert photos</p>
+              </TabPanel>
+            </TabPanels>
+        </Tabs> 
+      </Flex>
+
+        
+
+
+     
+
+
+  
+    
+      
+      
+    </ChakraProvider>
   );
 }
+
 
 export default App;
